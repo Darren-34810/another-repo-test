3890bee65a89788a8966451c2d554cc3bbcc3e1a
Darren-34810 <darren.denisson@student.umn.ac.id>
2021-05-02 23:08:39 +0700

    Memasukkan struktur dalam index.php (1)

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width">
+    <title>Coba git remote</title>
+</head>
+
+<body>
+    <h1>Hello World!</h1>
+    <ul>
+        <li>
+            <a>List one</a>
+            <a>List two</a>
+            <a>List three</a>
+        </li>
+    </ul>
+</body>
+
+</html>
